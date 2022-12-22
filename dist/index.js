7e53ab17df9be3117322d8b78c13094379ee3a55
DESKTOP-0875TLU\issam <samydob@gmail.com>
2022-12-21 19:48:52 -0500

    can now connect to mongodb

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const dotenv = require("dotenv");
+const MongoDBConnection_1 = require("./mongodb/MongoDBConnection");
+dotenv.config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.API_PORT;
 app.get("/", (req, res) => {
     res.send("Hi issam");
 });
 app.listen(PORT, () => {
-    console.log(`API RUNNNING AT PORT ${PORT}`);
+    const c = MongoDBConnection_1.connection;
+    console.log(`API RUNNNING AT PORT ${PORT} in ${process.env.NODE_ENV}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import * as express from "express";
+import * as dotenv from "dotenv";
+import { connection, MongoDBConnection } from "./mongodb/MongoDBConnection";
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.API_PORT;
 
 app.get("/", (req, res) => {
   res.send("Hi issam");
 });
 
 app.listen(PORT, () => {
-  console.log(`API RUNNNING AT PORT ${PORT}`);
+  console.log(`API RUNNNING AT PORT ${PORT} in ${process.env.NODE_ENV}`);
 });
