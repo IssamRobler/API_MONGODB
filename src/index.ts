ae8b325cbcb7163ef48787c9b950b3c83ddafa39
DESKTOP-0875TLU\issam <samydob@gmail.com>
2022-12-25 18:09:16 -0500

    setup test

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ import {
   errorMiddleware,
 } from "./common/middleware/error.middleware";
 import userRouter from "./controllers/user/user.controller";
+import { db } from "./mongodb/MongoDBConnection";
 dotenv.config();
-
 const app = express();
 
 app.use(
@@ -30,6 +30,18 @@ app.use("/user", userRouter);
 app.use(errorMiddleware);
 app.use(critialErrorMiddleware);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`API RUNNNING AT PORT ${PORT} in ${process.env.NODE_ENV}`);
 });
+
+process.on("SIGINT", async function () {
+  console.info("SIGINT signal received.");
+  console.log("Closing http server.");
+  server.close(() => {
+    console.log("Http server closed. Cleanup db");
+    // boolean means [force], see in mongoose do
+  });
+  await db.close()
+  process.exit(0);
+});
+export default server;
